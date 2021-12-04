24402e600c964bc222a52c9b90996354098e243c
ymikiii <1271311538@qq.com>
2021-12-04 22:58:32 +0800

    first commit

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,8 @@
       <component :is="component" />
     </div>
     <div class="demo-actions">
-      <Button @click="toggleCode">查看代码</Button>
+      <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+      <Button @click="showCode" v-else>查看代码</Button>
     </div>
     <div class="demo-code" v-if="codeVisible">
       <pre class="language-html" v-html="html" />
@@ -34,13 +35,15 @@ export default {
     const html = computed(() => {
       return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
     })
-    const toggleCode = () => codeVisible.value = !codeVisible.value
+    const showCode = () => codeVisible.value = true
+    const hideCode = () => codeVisible.value = false
     const codeVisible = ref(false)
     return {
       Prism,
       html,
       codeVisible,
-      toggleCode
+      showCode,
+      hideCode
     }
   }
 }
@@ -48,30 +51,24 @@ export default {
 
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
-
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
-
   >h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
   }
-
   &-component {
     padding: 16px;
   }
-
   &-actions {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
   }
-
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-
     >pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -3,3 +3,11 @@
     <slot />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+defineComponent
+export default {
+  name: 'GuluTab'
+}
+</script>
